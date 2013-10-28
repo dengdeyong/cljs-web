64d200f60623f96aea6022fefffc78b1bd8912b8
dengdeyong <chaoy2010@yeah.net>
2013-10-28 22:42:12 +0800

    一个简单的Slider效果

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject cljs-web "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1586"]
+  				 [jayq "2.4.0"]]
+  :plugins [[lein-cljsbuild "0.2.7"]
+  			[lein-swank "1.4.4"]]
+  :cljsbuild {
+  		:repl-listen-port 9000
+        :builds [{
+          :source-path "src/cljs"
+          :compiler {
+            :output-to "resources/public/js/core.js"
+            :optimizations :whitespace
+            :pretty-print true}}]})
---
+++ src/cljs/cljs_web/core.cljs
@@ -0,0 +1,44 @@
+(ns cljs-web.core
+(:require [clojure.browser.repl :as repl]
+          [jayq.core :as jq]))
+            
+(repl/connect "http://localhost:9000/repl")
+
+(defn- get-client-width
+  []
+  (jq/width (jq/$ :body)))
+
+(def content-id "#j_content")
+(def content-wrap "#j_content .content-wrap")
+(def part-count (count (jq/$ content-wrap)))
+(def client-width (get-client-width))
+
+(defn init-ui
+  "初始化内容区宽度，并设置宽度"
+  []
+  (let [el-content-wrap (jq/$ content-wrap)
+        el-content-main (jq/$ content-id)]
+   (jq/css el-content-wrap {:width (str client-width "px")})
+   (jq/css el-content-main {:width (str (* part-count client-width) "px")})))
+
+(defn slider
+  "内容移动方法，elem为内容所在区dom对象，index为当前内容在内容区中的索引"
+  [elem index]
+  (jq/anim elem {:left  (str (- 0 (* client-width index)) "px")} "easeInOutExpo"))
+
+(defn nav
+  "导航效果，el-navs为导航菜单dom对象,为一个集合"
+  [el-navs]
+  (doall
+   (map (fn [el-nav el-index]
+          (let [elem-nav (jq/$ el-nav)]
+            (jq/bind elem-nav :click (fn []
+                                       (slider (jq/$ content-id) el-index)
+                                       (jq/remove-class el-navs "active")
+                                       (jq/add-class elem-nav "active")))))
+        el-navs
+        (iterate inc 0)))
+  (jq/trigger (jq/$ (first el-navs)) :click))
+
+(init-ui)
+(nav (jq/$ "#j_menunav li a"))
