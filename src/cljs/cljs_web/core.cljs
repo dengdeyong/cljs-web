e0af375d11deddc12edfb77f2999051dc69fd337
dengdeyong <chaoy2010@yeah.net>
2013-10-31 16:25:04 +0800

    代码优化

--- src/cljs/cljs_web/core.cljs
+++ src/cljs/cljs_web/core.cljs
@@ -23,21 +23,28 @@
 
 (defn slider
   "内容移动方法，elem为内容所在区dom对象，index为当前内容在内容区中的索引"
-  [elem index]
+  [elem index client-width]
   (jq/anim elem {:left  (str (- 0 (* client-width index)) "px")} "easeInOutExpo"))
+
+(defn nav-event
+  [elem-navs elem-nav elem-index]
+  (slider (jq/$ content-id) elem-index client-width)
+  (jq/remove-class elem-navs "active")
+  (jq/add-class elem-nav "active"))
+
+(defn bind-evt
+  [el-navs el-nav el-index]
+  (let [elem-nav (jq/$ el-nav)]
+    (jq/bind elem-nav
+             :click
+             (fn []
+               (nav-event el-navs elem-nav el-index)))))
 
 (defn nav
   "导航效果，el-navs为导航菜单dom对象,为一个集合"
   [el-navs]
   (doall
-   (map (fn [el-nav el-index]
-          (let [elem-nav (jq/$ el-nav)]
-            (jq/bind elem-nav :click (fn []
-                                       (slider (jq/$ content-id) el-index)
-                                       (jq/remove-class el-navs "active")
-                                       (jq/add-class elem-nav "active")))))
-        el-navs
-        (iterate inc 0)))
+   (map (partial bind-evt el-navs) el-navs (iterate inc 0)))
   (jq/trigger (jq/$ (first el-navs)) :click))
 
 (init-ui)
