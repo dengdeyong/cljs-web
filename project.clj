5834f5a0f5183f256a42d12e3da7d2116bba5f12
dengdeyong <chaoy2010@yeah.net>
2013-11-12 21:09:17 +0800

    修改PROJECT

--- project.clj
+++ project.clj
@@ -4,10 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1586"]
-                 [webfui "0.2.1"]
-                 [noir "1.3.0-beta10"]
-                 [hiccup "1.0.1"]]
+                 [org.clojure/clojurescript "0.0-1586"]]
   :plugins [[lein-cljsbuild "0.2.7"]
             [lein-swank "1.4.4"]]
   :cljsbuild {
@@ -16,5 +13,5 @@
                         :source-path "src/cljs"
                         :compiler {
                                    :output-to "resources/public/js/core.js"
-                                   :optimizations :whitespace
+                                   :optimizations :advanced
                                    :pretty-print true}}]})
