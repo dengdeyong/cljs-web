ee587fb0bb4561ad90e43c7daee02ee3220de84f
dengdeyong <chaoy2010@yeah.net>
2013-11-14 22:22:14 +0800

    enfocus实现Slider

--- project.clj
+++ project.clj
@@ -4,14 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1586"]]
-  :plugins [[lein-cljsbuild "0.2.7"]
-            [lein-swank "1.4.4"]]
+                 [ring/ring-core "1.1.8"]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [enfocus "2.0.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [lein-ring "0.8.3"]]
   :cljsbuild {
               :repl-listen-port 9000
               :builds [{
                         :source-path "src/cljs"
                         :compiler {
                                    :output-to "resources/public/js/core.js"
-                                   :optimizations :advanced
+                                   :optimizations :whitespace
                                    :pretty-print true}}]})
--- src/cljs/cljs_web/core.cljs
+++ src/cljs/cljs_web/core.cljs
@@ -1,50 +1,46 @@
 (ns cljs-web.core
-  (:use [webfui.framework :only [launch-app]]
-        [webfui.utilities :only [get-attribute clicked]])
-  (:use-macros [webfui.framework.macros :only [add-mouse-watch]]))
-
-(def initial-state {:position 0})
+  (:require [enfocus.core :as ef]
+            [enfocus.effects :as effects]
+            [enfocus.events :as events]
+            [clojure.browser.repl :as repl])
+  (:require-macros [enfocus.macros :as em]))
 
 (defn get-client-width []
   (.-clientWidth (.-body js/document)))
 
-(def menu-nav [[:1 "网站首页" :nav]
-               [:2 "关于我们" :nav]
-               [:3 "业务体系" :nav]
-               [:4 "人才招聘" :nav]
-               [:5 "公司动态" :nav]
-               [:6 "联系我们" :nav]])
+(defn get-screen-width []
+  (let [client-width (get-client-width)]
+    (str client-width "px")))
 
-(defn display-width
-  [screen-count]
-  (str (* (get-client-width) screen-count) "px"))
+(defn get-container-width []
+  (let [client-width (get-client-width)]
+    (str (* client-width 7) "px")))
 
-(defn render-all [state]
-  [:div {:class :main}
-   [:div {:class :mainbg}
-    [:div.mainbg1]
-    [:div.mainbg2]]
-   [:div {:class :content
-          :style {:left (str (:position state) "px")
-                  :width (display-width (count menu-nav))}}
-    (for [[index label mouse] menu-nav]
-      [:div {:class :content-wrap
-             :style {:width (display-width 1)}}
-       [:div {:class :content-in}
-        [:h1 label]]])]
-   [:div {:class :menu-nav}
-    [:ul {:class :ul-nav}
-     (for [[index label mouse] menu-nav]
-       [:li
-        [:a {:href "javascript:;"}
-         [:span {:mouse mouse :rel index} label]]])]]])
+(em/defaction init-size []
+  ["#content"] (ef/set-style :width (get-container-width))
+  ["#content > *.content-wrap"] (ef/set-style :width (get-screen-width)))
 
-(add-mouse-watch :nav [state first-element last-element]
-                 (when (clicked first-element last-element)
-                   (let [client-width (get-client-width)
-                         nav-index (name (get-attribute first-element :rel))
-                         move-left (* client-width (- nav-index 1))]
-                     {:position (* -1 move-left)})))
+(em/defaction remove-nav-active []
+  "#menunav > * a" (ef/remove-class "active"))
 
-(launch-app (atom initial-state) render-all)
+(em/defaction move-content [move-left]
+  "#content" (effects/chain
+              (effects/move (* -1 move-left) :cury 500)))
 
+(defn set-current-nav [target]
+  (ef/at target (ef/add-class "active")))
+
+(defn set-nav [target]
+  (let [cur-index (ef/from target (ef/get-attr :rel))
+        move-left (* (js/parseInt cur-index) (get-client-width))]
+    (remove-nav-active)
+    (set-current-nav target)
+    (move-content move-left)))
+
+(em/defaction nav []
+  ["#menunav > * a"] (events/listen :click
+                                    #(set-nav (.-currentTarget %))))
+
+(set! (.-onload js/window)
+      #(do (init-size)
+           (nav)))
